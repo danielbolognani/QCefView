a7f8cefc7a7afd415d8bb4ec50c4378ef90aea3c
danielbolognani <bamboo@MAC-ACACIO.local>
2022-11-24 14:57:21 -0300

    Corrigindo build estatico MacOS e alterando create do Browser para Sincrono

--- generate-mac-x86_64.sh
+++ generate-mac-x86_64.sh
@@ -4,7 +4,7 @@
 
 BUILD_PROJECT=0
 
-BUILD_DIR="$(pwd)/.build/macos.x86_64"
+BUILD_DIR="$(pwd)/.build/macos.x86_64.release"
 
 while getopts bi flag
 do
@@ -14,10 +14,10 @@ do
 done
 
 echo ============== Config project ==============
-cmake -G "Xcode" -S . -B "${BUILD_DIR}" -DPROJECT_ARCH=x86_64 -DBUILD_DEMO=ON -DUSE_SANDBOX=ON -DCMAKE_INSTALL_PREFIX:PATH="$(pwd)/out/macos.x86_64"
+cmake -G "Xcode" -S . -B "${BUILD_DIR}" -DPROJECT_ARCH=x86_64 -DBUILD_DEMO=ON -DUSE_SANDBOX=ON -DCMAKE_INSTALL_PREFIX:PATH="$(pwd)/out/macos.x86_64" -DOPENSSL_INCLUDE_DIR=/Users/bamboo/Qt/6.2.5/depends/include -DCMAKE_BUILD_TYPE=Release -DICU_ROOT=/Users/bamboo/Qt/6.2.5/depends -DICU_INCLUDE_DIRS=/Users/bamboo/Qt/6.2.5/depends/include
 
 if [ ${BUILD_PROJECT} -eq 1 ] 
 then
     echo ============== Build project ==============
-    cmake --build "${BUILD_DIR}"
+    cmake --build "${BUILD_DIR}" --config Release
 fi
